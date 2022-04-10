cfaa2b20b17dec29b76026948e26d8033cb7d49f
LodGvedeon <drovosekov1999@mail.ru>
2022-04-10 17:25:59 +0300

    Readind excel

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+print('Started script')
+
+p = '/home/jaims/zheka/diplomNeuro/materials/positive.csv' 
+p_test = '/home/jaims/zheka/diplomNeuro/materials/test.csv' 
+print('Start reading data...')
+data_raw = pd.read_csv(p, error_bad_lines=False)
+print('data readed')
+data = pd.DataFrame(data = data_raw)
+first_ten = data.head(10)
+t = data.iloc[0]
+print(t)
