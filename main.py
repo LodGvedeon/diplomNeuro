0f24f5eac477636268ab1ded328855398393166b
LodGvedeon <drovosekov1999@mail.ru>
2022-04-12 00:56:31 +0300

    ok

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+print('Imports starting')
 from keras.datasets import imdb   
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM
 from keras.preprocessing.sequence import sequence
 
-
+print('Starting script')
 max_words = 10000
 
 maxlen = 32
 
 (x_train, y_train), (_, _) = imdb.load_data(num_words=max_words)
+
+print(x_train[:2])
 
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 
