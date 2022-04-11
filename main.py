44f9fe1c64509f6e03570a81dfbb3fdeeb28b2c0
LodGvedeon <drovosekov1999@mail.ru>
2022-04-12 00:31:22 +0300

    Try to dwnl data

---
+++ main.py
@@ -0,0 +1,20 @@
+from keras.datasets import imdb   
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+from keras.preprocessing.sequence import sequence
+
+
+max_words = 10000
+
+maxlen = 32
+
+(x_train, y_train), (_, _) = imdb.load_data(num_words=max_words)
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+
+print(y_train[:10])
+
+model = Sequential()
+model.add(Embedding(max_words, 16))
+model.add(LSTM(128))
+model.add(Dense(1, activation='sigmoid'))
